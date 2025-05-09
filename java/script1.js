3e8d078003ade203a90398712405567558fe77e1
Ana Clara <ana.penachio@escola.pr.gov.br>
2025-05-09 11:00:19 -0300

    Add files via upload

---
+++ java/script1.js
@@ -0,0 +1,21 @@
+const container = document.querySelector(".container");
+
+const numHegonos = 200;
+
+const colors = ['#007bff', '28a745', 'ffc107'];
+
+function getRandomNumber(min, max) {
+    return Math.random()*(max-min) + min;
+}
+
+function createHexagon(){
+    const hexagon = document.createElement('div');
+    hexagon.classList.add('hexagon');
+
+    const x = getRandomNumber(0, window.innerWidth - 50);
+    const y = getRandomNumber(0, window.innerHeight - 57.735);
+    hexagon.style.left = `${x}px;
+    hexagon.style.top = `${y}px;
+
+    
+}
